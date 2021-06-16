62d326ee8c5e7642a216f40d86d60f8d6c76048e
Joaquim Viana <jviana@unilurio.ac.mz>
2021-06-16 18:42:40 +0200

    initial commit

--- app.py
+++ app.py
@@ -4,8 +4,19 @@ app = Flask(__name__)
 
 @app.get('/')
 def index():
-    return '<h1> It works fine!</h1>'
-
+    return '''
+    <html>
+        <head>
+            <title>FCA</title>
+        </head>
+        <body>
+            <h1>Faculdade de Ciências Agrárias</h1>
+            <p>Página em manutenção. Voltaremos em breve.</p>
+            <br><br><br><br><br>
+            <marquee>UNILÚRIO - CIÊNCIA- DESENVOLVIMENTO - COMPROMISSO</marquee>
+        </body>
+    </html>
+    '''
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0')
